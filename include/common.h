42f0e29d991226f34efe32cc21de807aa8537629
Rainchus <rainmachus@gmail.com>
2022-04-21 10:49:07 -0500

    update various things

--- include/common.h
+++ include/common.h
@@ -1,17 +1,6 @@
-#ifndef _TYPES_H_
-#define _TYPES_H_
+#ifndef _COMMON_H_
+#define _COMMON_H_
 
-#define NULL 0
-
-typedef signed char             s8;
-typedef unsigned char           u8;
-typedef signed short int        s16;
-typedef unsigned short int      u16;
-typedef signed int              s32;
-typedef unsigned int            u32;
-typedef signed long long int    s64;
-typedef unsigned long long int  u64;
-typedef float                   f32;
-typedef double                  f64;
+#include "types.h"
 
 #endif
---
+++ include/types.h
@@ -0,0 +1,17 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#define NULL 0
+
+typedef signed char             s8;
+typedef unsigned char           u8;
+typedef signed short int        s16;
+typedef unsigned short int      u16;
+typedef signed int              s32;
+typedef unsigned int            u32;
+typedef signed long long int    s64;
+typedef unsigned long long int  u64;
+typedef float                   f32;
+typedef double                  f64;
+
+#endif
