5a13b87965aa659e2cfbe8c852a6610376fd8483
Rainchus <rainmachus@gmail.com>
2022-08-14 23:58:30 -0500

    ovl_48 complete

--- diff_settings.py
+++ diff_settings.py
@@ -5,3 +5,4 @@ def apply(config, args):
     config['myimg'] = 'build/marioparty.z64'
     config['mapfile'] = 'build/marioparty.map'
     config['source_directories'] = ['.']
+    config['makeflags'] = ['COMPARE=0']
--- src/ovl_48_BowserVisitDKBoard/285B70.c
+++ src/ovl_48_BowserVisitDKBoard/285B70.c
@@ -2,9 +2,7 @@
 #include "285B70.h"
 
 Vec3f D_800F70C0_286650 = {-115.0f, 0.0f, 1120.0f};
-Vec3f D_800F70CC_28665C = {30.0f, 350.0f, 1475.0f};
-Vec3f D_800F70D8_286668 = {30.0f, 65.0f, 1475.0f};
-
+Vec3f D_800F70CC_28665C[] = {{30.0f, 350.0f, 1475.0f},{30.0f, 65.0f, 1475.0f}};
 Vec3f D_800F70E4_286674 = {115.0f, 0.0f, 1520.0f};
 
 s32 D_800F70F0_286680[] = { 0x00000003, 0x000A0068, 0x000A006A, 0x000A006C };
@@ -22,11 +20,11 @@ s32* D_800F7148_2866D8[] = {D_800F7100_286690, D_800F710C_28669C, D_800F7118_286
 //f64 D_800F7160_2866F8 = 0.017453292519943295;
 
 //bss
-u8 D_800F7170_ovl48 = NULL;
-unk_Struct03* D_800F7174_ovl48 = NULL;
-unk_Struct03* D_800F7178_ovl48 = NULL;
-unk_Struct03* D_800F717C_ovl48 = NULL;
-Object* D_800F7180_ovl48 = NULL;
+extern u8 D_800F7170_ovl48;
+extern unk_Struct03* D_800F7174_ovl48;
+extern unk_Struct03* D_800F7178_ovl48;
+extern unk_Struct03* D_800F717C_ovl48;
+extern Object* D_800F7180_ovl48;
 
 void func_800F65E0_285B70(void) {
     D_800F7170_ovl48 = D_800ED5DD;
@@ -103,15 +101,15 @@ void func_800F66BC_285C4C(void) {
         func_8004F4D4(D_800F7174_ovl48, 2, 0);
         func_8004F40C(D_800F7174_ovl48, 0, 2);
         SleepProcess(0x14);
-        var_f20 = D_800F70CC_28665C.y;
+        var_f20 = D_800F70CC_28665C[0].y;
 
-        while (D_800F70D8_286668.y <= var_f20) {
+        while (D_800F70CC_28665C[1].y <= var_f20) {
             D_800F7178_ovl48->unk_30 = var_f20;
             SleepVProcess();
             var_f20 -= 2.0f;
         }
 
-        D_800F7178_ovl48->unk_30 = D_800F70D8_286668.y;
+        D_800F7178_ovl48->unk_30 = D_800F70CC_28665C[1].y;
         D_800F7180_ovl48->unk_4C = 1;
         SleepVProcess();
         func_8003E81C(D_800F7174_ovl48, 1, 0);
@@ -151,9 +149,9 @@ void func_800F6C14_2861A4(Object* arg0) {
     unk_Struct03* temp_s0_3;
 
     temp_s0 = D_800F7178_ovl48;
-    temp_s0->unk_18.x = func_80088060(arg0->unk_24 * (PI / 180.0));
+    temp_s0->unk_18.x = func_80088060(arg0->unk_24 * 0.017453292519943295);
     temp_s0_2 = D_800F7178_ovl48;
-    temp_s0_2->unk_18.z = func_800855C0(arg0->unk_24 * (PI / 180.0));
+    temp_s0_2->unk_18.z = func_800855C0(arg0->unk_24 * 0.017453292519943295);
     if ((arg0->unk_4C == 0) || !(arg0->unk_24 < -310.0f)) {
         arg0->unk_24 = arg0->unk_24 - 5.0f;
         if (arg0->unk_24 <= -360.0f) {
@@ -161,7 +159,7 @@ void func_800F6C14_2861A4(Object* arg0) {
         }
     } else {
         temp_s0_3 = D_800F7178_ovl48;
-        temp_s0_3->unk_30 = ((func_80088060(arg0->unk_28 * (PI / 180.0))) * 4.0f) + D_800F70D8_286668.y;
+        temp_s0_3->unk_30 = ((func_80088060(arg0->unk_28 * 0.017453292519943295)) * 4.0f) + D_800F70CC_28665C[1].y;
         arg0->unk_28 = arg0->unk_28 + 5.0f;
         if (arg0->unk_28 >= 360.0f) {
             arg0->unk_28 -= 360.0f;
@@ -198,10 +196,10 @@ void func_800F6E1C_2863AC(void) {
     D_800F7174_ovl48->unk_28 = 1.5f;
     D_800F7174_ovl48->unk_24 = 1.5f;
     D_800F7178_ovl48 = func_8003DBE0(0x28, NULL);
-    D_800F7178_ovl48->coords.x = D_800F70CC_28665C.x;
+    D_800F7178_ovl48->coords.x = D_800F70CC_28665C[0].x;
     D_800F7178_ovl48->coords.y = 0.0f;
-    D_800F7178_ovl48->unk_30 = D_800F70CC_28665C.y;
-    D_800F7178_ovl48->coords.z = D_800F70CC_28665C.z;
+    D_800F7178_ovl48->unk_30 = D_800F70CC_28665C[0].y;
+    D_800F7178_ovl48->coords.z = D_800F70CC_28665C[0].z;
     D_800F7180_ovl48 = func_8005D384(0x1000, 0, 0, -1, &func_800F6C14_2861A4);
     D_800F7180_ovl48->unk_24 = -12.0f;
     D_800F7180_ovl48->unk_28 = 0.0f;
