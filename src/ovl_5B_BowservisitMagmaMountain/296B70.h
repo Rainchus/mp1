d46ddd8051d87de6218eaf0afd2bd0e46bfb8ab2
Rainchus <rainmachus@gmail.com>
2022-08-16 19:52:32 -0500

    decomp ovl_5B w/ data

--- src/ovl_5B_BowservisitMagmaMountain/296B70.h
+++ src/ovl_5B_BowservisitMagmaMountain/296B70.h
@@ -8,20 +8,25 @@ void func_800F7404_297994(void);
 void func_800F7560_297AF0(void);
 void func_800F7074_297604(void);
 
-extern void* D_800F7590;
+
+void func_800410E8(u8);
+s32 func_80041624(u8);
+void func_8004DBD4(s32, u8);
+void func_80055994(u8, s32);
+
+//bss
+extern u16 D_800F5460[];
+extern unk_Struct03* D_800F7590;
 extern s32 D_800F7594;
-extern s16 D_800F7596;
 extern s32 D_800F7598;
-extern s16 D_800F759A;
 extern void* D_800F759C;
 extern Vec3f D_800F75A0;
 extern Vec3f D_800F75AC;
 extern Vec3f D_800F75B8;
 extern s32 D_800F75C4;
 extern Object* D_800F7620[];
+extern s32 D_800F7640;
 extern u8 D_800F7650;
 extern unk_Struct03* D_800F7654;
 extern unk_Struct03* D_800F7658;
 extern unk_Struct03* D_800F765C;
-extern u8 D_800F7650;
-extern u8 D_800F7650;
