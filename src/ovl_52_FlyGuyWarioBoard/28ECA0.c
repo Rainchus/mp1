e5d88ee5af3c87b9e07bce782659b1988697de1a
Rainchus <rainmachus@gmail.com>
2022-08-18 03:31:13 -0500

    ovl_49 and ovl_53 w/ data, more ovl_52 funcs

--- src/ovl_52_FlyGuyWarioBoard/28ECA0.c
+++ src/ovl_52_FlyGuyWarioBoard/28ECA0.c
@@ -31,15 +31,53 @@ INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F6DE0_28F4A0);
 
 INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F7070_28F730);
 
-INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F72DC_28F99C);
+void func_800F72DC_28F99C(Object* arg0) {
+    unk_Struct03* temp_s0 = D_800F8234;
 
-INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F7384_28FA44);
+    temp_s0->unk_30 = ((func_80088060((arg0->unk_28 * D_800F8210)) * arg0->unk_4C) + arg0->unk_1C);
+    arg0->unk_28 += 10.0f;
+    if (arg0->unk_28 >= 360.0f) {
+        arg0->unk_28 -= 360.0f;
+    }
+}
+
+void func_800F7384_28FA44(Object* arg0) {
+    unk_Struct03* temp_s0 = D_800F8070;
+
+    temp_s0->unk_30 = ((func_80088060((arg0->unk_28 * D_800F8218)) * arg0->unk_4C) + arg0->unk_1C);
+    arg0->unk_28 += 10.0f;
+    if (arg0->unk_28 >= 360.0f) {
+        arg0->unk_28 -= 360.0f;
+    }
+}
 
 INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F742C_28FAEC);
 
-INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F7D14_2903D4);
+void func_800F7D14_2903D4(void) {
+    if (func_80072718() == 0) {
+        func_800F8044_290704();
+        func_800F7F30_2905F0();
+        func_80054654();
+        func_80070ED4();
+        if (D_800F807C == 0) {
+            func_8005DFB8(1);
+            return;
+        }
+        func_8004F284();
+        func_8004F28C(0x52, D_800F807E);
+    }
+}
 
-INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F7D90_290450);
+void func_800F7D90_290450(Object* arg0) {
+    if (D_800F5144 != 0) {
+        if (D_800F807C != 0) {
+            func_800726AC(6, 8);
+        } else {
+            func_800726AC(1, 16);
+        }
+        arg0->func_ptr = &func_800F7D14_2903D4;
+    }
+}
 
 INCLUDE_ASM(s32, "ovl_52_FlyGuyWarioBoard/28ECA0", func_800F7DF0_2904B0);
 
--- src/ovl_52_FlyGuyWarioBoard/28ECA0.h
+++ src/ovl_52_FlyGuyWarioBoard/28ECA0.h
@@ -3,5 +3,15 @@
 void func_800F742C_28FAEC(void);
 void func_800F7DF0_2904B0(void);
 void func_800F7FA4_290664(void);
+void func_800F7D90_290450(Object*);
+void func_800F7F30_2905F0(void);
+void func_800F8044_290704(void);
+void func_800F7D14_2903D4(void);
+extern s32 D_800F807C;
+extern s16 D_800F807E;
+extern s32 D_800F807C;
 extern u8 D_800F8230;
-void func_800F7D90_290450(void);
+extern f64 D_800F8210;
+extern unk_Struct03* D_800F8234;
+extern unk_Struct03* D_800F8070;
+extern f64 D_800F8218;
