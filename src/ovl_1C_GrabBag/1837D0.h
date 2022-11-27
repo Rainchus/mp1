07543cdda9f7d8715eceb6d0e4f890f3ff6dc63e
M0liusX <marioamolina1996@gmail.com>
2022-11-26 21:12:14 -0600

    decomp initial couple grab bag funcs

---
+++ src/ovl_1C_GrabBag/1837D0.h
@@ -0,0 +1,13 @@
+#ifndef _1837D0_H_
+#define _1837D0_H_
+
+unkObjectStruct* D_800EDE70[6];   // Array of unkObjectStruct pointers?
+u16 D_800EE984;                   // Offst into D_800EDE70 array.
+void func_800F6C98_183E88();
+void func_800F66F8_1838E8();                           /* extern */
+void func_800F67BC_1839AC();                           /* extern */
+void func_800F6C08_183DF8();                           /* extern */
+void func_800F7260_184450();                           /* extern */
+void func_800F7680_184870();                           /* extern */
+
+#endif
