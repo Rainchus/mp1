adee55fee0725df4d331d96969ccfd327c581695
M0liusX <marioamolina1996@gmail.com>
2022-12-17 02:05:39 -0600

    Finish cleanup of swap chain code

--- src/engine/graphics.c
+++ src/engine/graphics.c
@@ -15,66 +15,46 @@ typedef struct {
 typedef struct combinedStruct {
     OSTask task;
     OSMesgQueue mesgQueue;
-} swapChainTask;
+} swapChainTaskQueue;
 
-extern swapChainTask D_800D5ED0;
-extern OSThread D_800D1D20; // OSThread
-extern u8 D_800D26D0[]; // thread stack pointer
+extern swapChainTaskQueue swapChainTask;
+extern OSThread swapChainThread;
+extern u8 swapChainStack[];
 
-extern graphicsMessage D_800ECE20[0x20]; // OSMesg ring buffer
-extern s16 D_800ECB2E; // ring buffer index
+extern graphicsMessage gMesgRingBuffer[0x20];
+extern s16 ringBufferIndex;
 
-extern void **D_800D5FE8; // Current frame buffer pool
-extern u16 D_800D5FEC; // Frame buffer pool size
-extern u16 D_800D5FEE; // unknown
+extern void **frameBufferPool;
+extern u16 frameBufferCount;
+extern u16 frameBufferSegmentID;
 
-/* gThread stacks? */
-extern u64 *D_800ECB34;
-extern u64 *D_800F09F0;
-extern u64 *D_800F2CBC;
+extern u64 *gThreadOutStackSize;
+extern u64 *gThreadYieldStack;
+extern u64 *gThreadOutStack;
 extern u64 *gThread3Stack;
 
-extern s32 D_800D5F10[]; // OSMesgQueue address?
-extern u8 D_800B1760; // rspboot address
-extern u8 D_800B1830; // rspboot address end
-extern Gfx D_800ED398[]; // yield data pointer
+extern u8 rspBootAddressStart;
+extern u8 rspBootAddressEnd;
+extern Gfx gTaskDataPointers[];
 
-extern s32 D_800ED0C4; // graphics state?
-extern void *D_800F3838; // next frameBuffer
-extern void *D_800ECB1C; // current frameBuffer
+extern s32 D_800ED0C4;
+extern void *nextFrameBuffer;
+extern void *currFrameBuffer;
 
-extern s32 *D_800F317C; // graphics state?
-extern u32 D_800D5FF0; // graphics state?
+extern s32 *gUCodeAddresses;
+extern u32 D_800D5FF0; // message count?
 
 
-extern OSMesgQueue D_800D5F18;
-extern void* D_800D5F30; // Initial Mesg
-extern s16 D_800ED3C2; // Amount of total messages D_800D5F18 currently has
+extern OSMesgQueue gSwapChainMesgQueue;
+extern void* gSwapChainInitMesg;
+extern s16 gSwapChainMesgTotal;
 
-extern OSMesgQueue D_800D5FB0;
-extern void* D_800D5FC8; // Initial Mesg
+extern OSMesgQueue gMesgQueue;
+extern void* gMesgQueueInitMesg;
 
 /* NEW */
-typedef struct {
-    s32 unk00;
-    OSMesgQueue *unk04;
-    s32 unk08;
-} unkSchedStruct;
-
-typedef struct {
-    u8 unk00[0x20];
-} unkGraphicsMessage2; // OSMesg
-
-void func_800639F8(unkSchedStruct*, OSMesgQueue*, s32);
-void func_80061414();
-void func_800613E8();
-
-extern u32 D_800F383C;
-extern u32 D_800ED3A0;
-extern u32 D_800ED3A4;
-
-extern void *D_800ED39C; // frameBuffer pointer
-extern OSMesgQueue D_800ED3C8;
+extern u32 D_800F383C; // Unk
+extern OSMesgQueue D_800ED3C8; // Another system's message queue
 /* NEW */
 
 
@@ -85,27 +65,27 @@ void func_8001A600(void **arg1, s32 arg2, s32 arg3, u64 **arg4, s32 *arg5) {
     func_8001ABE8(arg5);
     func_8001ABF4(0);
 
-    D_800D5ED0.mesgQueue.mtqueue = (OSThread*) &D_800D5FB0;
-    D_800D5ED0.task.t.type = 1;
-    D_800D5ED0.task.t.flags = 2;
-    D_800D5ED0.task.t.ucode_boot = (u64*) &D_800B1760;
-    D_800D5ED0.task.t.ucode_boot_size = (s32) (&D_800B1830 - &D_800B1760);
-    D_800D5ED0.task.t.ucode_size = 0x1000;
-    D_800D5ED0.task.t.ucode_data_size = 0x800;
-    D_800D5ED0.task.t.dram_stack_size = 0x400;
-    D_800D5ED0.task.t.data_ptr = (u64*) &D_800ED398;
-    D_800D5ED0.task.t.data_size = 2;
-    D_800D5ED0.task.t.yield_data_size = 0xC00;
+    swapChainTask.mesgQueue.mtqueue = (OSThread*) &gMesgQueue;
+    swapChainTask.task.t.type = 1;
+    swapChainTask.task.t.flags = 2;
+    swapChainTask.task.t.ucode_boot = (u64*) &rspBootAddressStart;
+    swapChainTask.task.t.ucode_boot_size = (s32) (&rspBootAddressEnd - &rspBootAddressStart);
+    swapChainTask.task.t.ucode_size = 0x1000;
+    swapChainTask.task.t.ucode_data_size = 0x800;
+    swapChainTask.task.t.dram_stack_size = 0x400;
+    swapChainTask.task.t.data_ptr = (u64*) &gTaskDataPointers;
+    swapChainTask.task.t.data_size = 2;
+    swapChainTask.task.t.yield_data_size = 0xC00;
 
-    D_800ED3C2 = 0;
-    D_800ECB2E = 0;
+    gSwapChainMesgTotal = 0;
+    ringBufferIndex = 0;
     D_800ED0C4 = 0;
-    D_800F3838 = 0;
-    D_800ECB1C = 0;
-    osCreateMesgQueue(&D_800D5F18, &D_800D5F30, 0x20);
-    osCreateMesgQueue(&D_800D5FB0, &D_800D5FC8, 8);
-    osCreateThread(&D_800D1D20, 0x10, &func_8001A7DC, NULL, &D_800D26D0, 0x64);
-    osStartThread(&D_800D1D20);
+    nextFrameBuffer = 0;
+    currFrameBuffer = 0;
+    osCreateMesgQueue(&gSwapChainMesgQueue, &gSwapChainInitMesg, 0x20);
+    osCreateMesgQueue(&gMesgQueue, &gMesgQueueInitMesg, 8);
+    osCreateThread(&swapChainThread, 0x10, &func_8001A7DC, NULL, &swapChainStack, 0x64);
+    osStartThread(&swapChainThread);
 }
 
 /* Retrieve available frame buffer (triple buffering?) */
@@ -118,8 +98,8 @@ void *func_8001A750(void) {
     current = osViGetCurrentFramebuffer();
     next = osViGetNextFramebuffer();
     
-    for (i = 0; i < D_800D5FEC; i++) {
-        fBuffer = D_800D5FE8[i];
+    for (i = 0; i < frameBufferCount; i++) {
+        fBuffer = frameBufferPool[i];
         if (fBuffer != current) {
             if (fBuffer != next) {
                 return fBuffer;
@@ -129,80 +109,78 @@ void *func_8001A750(void) {
     return NULL;
 }
 
-// INCLUDE_ASM(s32, "../src/engine/graphics", func_8001A7DC);
-/* Swap chain thread func? */
+/* Swap Chain Loop */
 void func_8001A7DC(void* arg0) {
     unkSchedStruct sp10;
-    OSMesgQueue sp20;
+    OSMesgQueue mesgQueue;
     unkGraphicsMessage2 sp38;
     graphicsMessage* recvdMesg;
-    void* temp_v0; // available frameBuffer
+    void* pAvailableFrameBuffer; // available frameBuffer
     s32 var_s4;
     u32 i;
 
-    /* Why use temps? */
-    swapChainTask *pSwapChain;
-    OSTask        *pTask;
+    swapChainTaskQueue *pSwapChain;
+    OSTask *pTask;
 
     recvdMesg = NULL;
-    pTask = &D_800D5ED0.task;
-    pSwapChain = &D_800D5ED0;
+    pTask = &swapChainTask.task;
+    pSwapChain = &swapChainTask;
     var_s4 = 0;
 
-    osCreateMesgQueue(&sp20, (OSMesg*) &sp38, 8);
-    func_800639F8(&sp10, &sp20, 1);
+    osCreateMesgQueue(&mesgQueue, (OSMesg*) &sp38, 8);
+    func_800639F8(&sp10, &mesgQueue, 1);
 
     while (TRUE) {
         do {
             recvmesg:
-            osRecvMesg(&sp20, NULL, 1);
+            osRecvMesg(&mesgQueue, NULL, 1);
             if ((D_800F383C - var_s4) < D_800D5FF0) {
                 goto recvmesg;
             }
     
             var_s4 = D_800F383C;
             func_800613E8();
-            temp_v0 = func_8001A750();
-        } while (temp_v0 == NULL);
+            pAvailableFrameBuffer = func_8001A750();
+        } while (pAvailableFrameBuffer == NULL);
     
-        D_800ECB1C = D_800F3838;
-        D_800F3838 = temp_v0;
+        currFrameBuffer = nextFrameBuffer;
+        nextFrameBuffer = pAvailableFrameBuffer;
 
-        /* WHY EXTRA DO? */
+        /* TODO: WHY EXTRA DO? */
         do { do {
             u32 intMask;
-            Gfx *dl = D_800ED398;
-            osRecvMesg(&D_800D5F18, (OSMesg) &recvdMesg, 1);
-            gSPSegment(dl++, D_800D5FEE, temp_v0);
+            Gfx *dl = gTaskDataPointers;
+            osRecvMesg(&gSwapChainMesgQueue, (OSMesg) &recvdMesg, 1);
+            gSPSegment(dl++, frameBufferSegmentID, pAvailableFrameBuffer);
             gSPBranchList(dl++, recvdMesg->unk00);
             pSwapChain->mesgQueue.fullqueue = (void *) recvdMesg->unk08;
             pTask->t.dram_stack = gThread3Stack;
-            pTask->t.output_buff = D_800F2CBC;
-            pTask->t.output_buff_size = D_800ECB34;
-            pTask->t.yield_data_ptr = D_800F09F0;
-            pTask->t.ucode =      (u64 *) *(   (recvdMesg->unk04 * 2)      + D_800F317C); // offset     (data pairs?)
-            pTask->t.ucode_data = (u64 *) *( ( (recvdMesg->unk04 * 2) | 1) + D_800F317C); // offset + 1
+            pTask->t.output_buff = gThreadOutStack;
+            pTask->t.output_buff_size = gThreadOutStackSize;
+            pTask->t.yield_data_ptr = gThreadYieldStack;
+            pTask->t.ucode =      (u64 *) *(   (recvdMesg->unk04 * 2)      + gUCodeAddresses); // offset     (data pairs?)
+            pTask->t.ucode_data = (u64 *) *( ( (recvdMesg->unk04 * 2) | 1) + gUCodeAddresses); // offset + 1
 
             osSendMesg(&D_800ED3C8, (OSMesg) pSwapChain, 1);
-            osRecvMesg(&D_800D5FB0, NULL, 1);
+            osRecvMesg(&gMesgQueue, NULL, 1);
 
             intMask = osSetIntMask(1);
-            D_800ED3C2 -= 1;
+            gSwapChainMesgTotal -= 1;
             osSetIntMask(intMask);
 
             if (recvdMesg->unk0C != NULL) {
                 osSendMesg(recvdMesg->unk0C, (OSMesg) recvdMesg->unk10, 1);
             }
-        } while (!((s32) recvdMesg->unk08 & 1));
+        } while (!((s32) recvdMesg->unk08 & 1)); } while (FALSE);
     
         for (i = 1; i < D_800D5FF0; i++) {
-            osRecvMesg(&sp20, NULL, 1);
+            osRecvMesg(&mesgQueue, NULL, 1);
         }
     
-        osViSwapBuffer(temp_v0);
+        osViSwapBuffer(pAvailableFrameBuffer);
         if (D_800ED0C4 == 0) {
             osViBlack(0);
-        } } while (FALSE);
+        }
         D_800ED0C4++;
         func_80061414(); // empty function
     }
@@ -210,25 +188,25 @@ void func_8001A7DC(void* arg0) {
 
 /* Retrieve graphics OSMesg from ring buffer */
 graphicsMessage* func_8001AA70(void) {
-    if (D_800ECB2E >= 0x20) {
-        D_800ECB2E = NULL;
+    if (ringBufferIndex >= 0x20) {
+        ringBufferIndex = NULL;
     }
-    return &D_800ECE20[D_800ECB2E++];
+    return &gMesgRingBuffer[ringBufferIndex++];
 }
 
 /* Send graphics OSMesg */
 u8 func_8001AAC4(s32 arg0, u16 arg1, s32 arg2, OSMesgQueue *arg3, s32 arg4) {
     graphicsMessage* mesg;
-    u32 temp_v0;
+    u32 intMask;
 
-    if (D_800ED3C2 >= 0x20) {
+    if (gSwapChainMesgTotal >= 0x20) {
         return FALSE;
     }
 
-    temp_v0 = osSetIntMask(1);
-    D_800ED3C2++;
-    osSetIntMask(temp_v0);
-    mesg = func_8001AA70(); // Get OSMesg from ring buffer
+    intMask = osSetIntMask(1);
+    gSwapChainMesgTotal++;
+    osSetIntMask(intMask);
+    mesg = func_8001AA70();
 
     /* Initialize message. */
     mesg->unk00 = arg0;
@@ -237,36 +215,36 @@ u8 func_8001AAC4(s32 arg0, u16 arg1, s32 arg2, OSMesgQueue *arg3, s32 arg4) {
     mesg->unk0C = arg3;
     mesg->unk10 = arg4;
 
-    osSendMesg(&D_800D5F18, mesg, 1);
+    osSendMesg(&gSwapChainMesgQueue, mesg, 1);
     return TRUE;
 }
 
 /* Init graphics frame buffer pool */
-void func_8001AB84(void **frameBufferPool, u16 size, u16 arg2) {
-    D_800D5FE8 = frameBufferPool;
-    D_800D5FEC = size;
-    D_800D5FEE = arg2;
+void func_8001AB84(void **pool, u16 size, u16 segment) {
+    frameBufferPool = pool;
+    frameBufferCount = size;
+    frameBufferSegmentID = segment;
 }
 
 /* Reset frame buffer pool size. (Switch between double and triple buffering) */
 void func_8001ABA0(u16 size) {
-    D_800D5FEC = size;
+    frameBufferCount = size;
 }
 
 /* Initialize gThreadStacks */
-void func_8001ABAC(u64 **arg0) {
-    gThread3Stack = *arg0++;
-    D_800F2CBC = *arg0++;
-    D_800ECB34 = *arg0++;
-    D_800F09F0 = *arg0;
+void func_8001ABAC(u64 **threadStacks) {
+    gThread3Stack = *threadStacks++;
+    gThreadOutStack = *threadStacks++;
+    gThreadOutStackSize = *threadStacks++;
+    gThreadYieldStack = *threadStacks;
 }
 
-/* Graphics state init? */
-void func_8001ABE8(s32 *arg0) {
-    D_800F317C = arg0;
+/* Set pointer to swapchain task's ucode addresses */
+void func_8001ABE8(s32 *uCodeAdresses) {
+    gUCodeAddresses = uCodeAdresses;
 }
 
-/* Graphics state init? */
+/* Swap chain state set? */
 void func_8001ABF4(u32 arg0) {
     D_800D5FF0 = arg0;
 }
