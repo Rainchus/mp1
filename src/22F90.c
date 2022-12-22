d14f6d59bd83b052afe9cdb0ec04f0b97b7d9c78
M0liusX <marioamolina1996@gmail.com>
2022-12-22 15:50:50 -0600

    decomp scale and translate mtx funcs

--- src/22F90.c
+++ src/22F90.c
@@ -1,9 +1,29 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "22F90", func_80022390);
+typedef f32 Mat4[4][4];
 
-INCLUDE_ASM(s32, "22F90", func_80022450);
 
+/* Translate Matrix. */
+void func_80022390(Mat4 mtx, f32 x, f32 y, f32 z) {
+     mtx[3][0] = (x * mtx[0][0]) + (y * mtx[1][0]) + (z * mtx[2][0]) + mtx[3][0];
+     mtx[3][1] = (x * mtx[0][1]) + (y * mtx[1][1]) + (z * mtx[2][1]) + mtx[3][1];
+     mtx[3][2] = (x * mtx[0][2]) + (y * mtx[1][2]) + (z * mtx[2][2]) + mtx[3][2];
+     mtx[3][3] = (x * mtx[0][3]) + (y * mtx[1][3]) + (z * mtx[2][3]) + mtx[3][3];
+}
+
+
+/* Scale Matrix */
+void func_80022450(Mat4 mtx, f32 sx, f32 sy, f32 sz) {
+    s32 i;
+    for (i = 0; i < 3; i++) {
+        mtx[0][i] *= sx;
+        mtx[1][i] *= sy;
+        mtx[2][i] *= sz;
+    }
+}
+
+/* Rotate Matrix */
+void func_800224A4(float* mtx, f32 pitch, f32 yaw, f32 row); // Mat4 mtx
 INCLUDE_ASM(s32, "22F90", func_800224A4);
 
 INCLUDE_ASM(s32, "22F90", func_800227C4);
