181434156b8f719e524b75a7d70939476a9ae7ad
Rainchus <rainmachus@gmail.com>
2023-04-22 21:46:06 -0500

    ovl_43 added

---
+++ src/ovl_43_UnknownEndGameBonuses1/27CB30.c
@@ -0,0 +1,95 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F65E0_27CB30);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F667C_27CBCC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F6730_27CC80);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F6750_27CCA0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4A0_2809F0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4A4_2809F4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4B4_280A04);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4B8_280A08);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4BC_280A0C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4CC_280A1C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4EC_280A3C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4F0_280A40);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA4F4_280A44);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA51C_280A6C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA54C_280A9C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA558_280AA8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA564_280AB4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA63C_280B8C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA654_280BA4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA658_280BA8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA65C_280BAC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA660_280BB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F67EC_27CD3C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F6FC8_27D518);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F70D0_27D620);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F74E4_27DA34);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F7580_27DAD0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F7918_27DE68);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA6C8_280C18);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F79B4_27DF04);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA6D8_280C28);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA6DC_280C2C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA6E0_280C30);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA6F0_280C40);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA700_280C50);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA704_280C54);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA714_280C64);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", D_800FA718_280C68);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F7DE0_27E330);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F9E18_280368);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F9E44_280394);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F9E78_2803C8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F9ED8_280428);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800F9F1C_28046C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800FA264_2807B4);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800FA3C0_280910);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_43_UnknownEndGameBonuses1/27CB30", func_800FA474_2809C4);
