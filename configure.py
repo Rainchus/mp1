6e96fd82de9dff881985b43acbbfb43cdf17acd2
Rainchus <rainmachus@gmail.com>
2023-05-20 00:22:46 -0500

    update ninja build system

--- configure.py
+++ configure.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
+import ninja_syntax
 
 dir_path = 'src/'
 asm_path = 'asm/'
 assets_path = 'assets/'
 optO0_files = ['synsetpan.c', 'synstartvoiceparam.c', 'A27D0.c', 'ABCD0.c', 'ACA90.c', 'ACCB0.c', 'ACF80.c', 'AD380.c', 'AD740.c', 'ADA70.c', 'ADBD0.c', 'ADF70.c', 'AE150.c', 'AE630.c', 'AE820.c', 'AED10.c', 'AEF30.c', 'AF450.c', 'AF6C0.c', 'AF960.c', 'AFBD0.c', 'AFE60.c', 'AFF70.c', 'B07B0.c', 'B0BA0.c', 'B0FC0.c', 'B1930.c', 'B1B60.c', 'B1DC0.c', 'B1FD0.c', 'B22E0.c', 'B2310.c', '89EA0.c', 'A2080.c', 'A21C0.c', 'A3370.c']
+optO2_files = ['95F40.c']
 optO3_files = ['7CD60.c']
 misc_files = ["48D90.c", "3AC60.c", "math.c"]
 
@@ -18,18 +20,6 @@ else:
         DETECTED_OS = 'macos'
         MAKE = 'gmake'
         CPPFLAGS += '-xc++'
-
-# fix patch applied by mod_configure if it exist
-# open the file for reading
-# with open('src/engine/gameman.c', 'r') as f:
-#     filedata = f.read()
-
-# # replace the string 'cPerFrameFunction' with 'SleepVProcess'
-# newdata = filedata.replace('cPerFrameFunction', 'SleepVProcess')
-
-# # open the file for writing and write the updated content
-# with open('src/engine/gameman.c', 'w') as f:
-#     f.write(newdata)
 
 with open('build.ninja', 'w') as f:
     f.write(f'DETECTED_OS = {DETECTED_OS}\n')
@@ -56,60 +46,101 @@ header = (
     'OPTFLAGS = -O1\n'
     'MAKE_EXPECTED = tools/make_expected.py\n'
     '\n'
+)
 
-    'rule misc_cc\n'
-    '  command = (export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC $OPTFLAGS -G0 -mips3 -mgp32 -mfp32 -D_LANGUAGE_C $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)\n'
-    '\n'
+# Create a Ninja build file object
+with open('build.ninja', 'a') as file:
+    file.write(f'{header}')
+ninja_file = ninja_syntax.Writer(open('build.ninja', 'a'))
 
-    'rule lib_cc\n'
-    '  command = (export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O3 -funsigned-char -G0 -mips3 -mgp32 -mfp32 -D_MIPS_SZLONG=32 -D_LANGUAGE_C -DF3DEX_GBI -I include -I include/PR -I include/gcc -I build/include -I src -DNDEBUG -D_MIPS_SZLONG=32 -DF3DEX_GBI_2 -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)\n'
-    '\n'
+ninja_file.rule('misc_cc',
+                command = "(export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC $OPTFLAGS -G0 -mips3 -mgp32 -mfp32 -D_LANGUAGE_C $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)",
+                description = "Compiling misc files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    'rule O0_cc\n'
-    '  command = (export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O0 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)\n'
-    '\n'
+ninja_file.rule('lib_cc',
+                command = "(export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O3 -funsigned-char -G0 -mips3 -mgp32 -mfp32 -D_MIPS_SZLONG=32 -D_LANGUAGE_C -DF3DEX_GBI -I include -I include/PR -I include/gcc -I build/include -I src -DNDEBUG -D_MIPS_SZLONG=32 -DF3DEX_GBI_2 -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)",
+                description = "Compiling lib files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    'rule O2_cc\n'
-    '  command = (export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O2 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)\n'
-    '\n'
+ninja_file.rule('O0_cc',
+                command = "(export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O0 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)",
+                description = "Compiling O0 files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    'rule O3_cc\n'
-    '  command = (export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O3 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)\n'
-    '\n'
+ninja_file.rule('main_cc',
+                command = "(export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O1 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)",
+                description = "Compiling O1 files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    'rule main_cc\n'
-    '  command = (export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC $OPTFLAGS $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)\n'
-    '\n'
+ninja_file.rule('O2_cc',
+                command = "(export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O2 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)",
+                description = "Compiling O2 files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    'rule s_file\n'
-    '  command = $AS $ASFLAGS -o $out $in\n'
-    '\n'
+ninja_file.rule('O3_cc',
+                command = "(export COMPILER_PATH=tools/gcc_2.7.2/$DETECTED_OS && $CC -O3 $CFLAGS $CPPFLAGS -c -o $out $in) && ($STRIP $out -N dummy-symbol-name)",
+                description = "Compiling O3 files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    "rule bin_file\n"
-    "  command = $LD -r -b binary -o $out $in\n"
-    "\n"
+ninja_file.rule('s_file',
+                command = "$AS $ASFLAGS -o $out $in",
+                description = "Assembling s files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    "rule make_elf\n"
-    "  command = $LD $LDFLAGS -o $out\n"
-    "\n"
+ninja_file.rule('bin_file',
+                command = "$LD -r -b binary -o $out $in",
+                description = "Copying bin files...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    "rule make_rom_bin\n"
-    "  command = $OBJCOPY -O binary $in $out\n"
-    "\n"
+ninja_file.rule('make_elf',
+                command = "$LD $LDFLAGS -o $out",
+                description = "Creating elf file...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    "rule make_rom_z64\n"
-    "  command = (cp $in $out) && (sha1sum -c marioparty.sha1)\n"
-    "\n"
+ninja_file.rule('make_rom_bin',
+                command = "$OBJCOPY -O binary $in $out",
+                description = "Creating rom bin...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
-    "rule make_expected_folder\n"
-    "  command = (cp $in $out) && (python3 ./$MAKE_EXPECTED $in)\n"
-    "\n"
+ninja_file.rule('make_rom_z64',
+                command = "(cp $in $out) && (sha1sum -c marioparty.sha1)",
+                description = "Creating rom z64...",
+                depfile = "$out.d",
+                deps = "gcc"
+)
 
+ninja_file.rule('make_expected_folder',
+                command = "(cp $in $out) && (sha1sum -c marioparty.sha1)",
+                description = "Making expected folder...",
 )
 
 # Traverse each subdirectory recursively and find all C files
-def append_extension(filename):
-    return filename + '.o'
+def append_extension(filename, extension='.o'):
+    return filename + extension
+
+def append_prefix(filename, extension):
+    return extension + filename
 
 c_files = []
 for root, dirs, files in os.walk(dir_path):
@@ -136,48 +167,37 @@ for file in c_files + s_files + bin_files:
         if 'asm/nonmatchings/' not in file:
             o_files.append("build/" + append_extension(file))
 
-with open('build.ninja', 'a') as f:
-    f.write(header)
+# Write the rules for .c files
+for c_file in c_files:
+    if "src/mod" in os.path.relpath(c_file):
+        continue
+    if os.path.basename(c_file) in misc_files:
+        ninja_file.build("build/" + append_extension(c_file), "misc_cc", c_file)
+    elif os.path.basename(c_file) in optO0_files:
+        ninja_file.build("build/" + append_extension(c_file), "O0_cc", c_file)
+    elif os.path.basename(c_file) in optO3_files:
+        ninja_file.build("build/" + append_extension(c_file), "O3_cc", c_file)
+    elif "src/lib" in os.path.relpath(c_file):
+        ninja_file.build("build/" + append_extension(c_file), "lib_cc", c_file)
+    elif os.path.basename(c_file) in optO2_files:
+        ninja_file.build("build/" + append_extension(c_file), "O2_cc", c_file)
+    else:
+        ninja_file.build("build/" + append_extension(c_file), "main_cc", c_file)
 
-# Write the full path of each C file to a new text file called build.ninja
-with open('build.ninja', 'a') as outfile:
-    # Write the rules for the .c files
-    for c_file in c_files:
-        if "src/mod" in os.path.relpath(c_file):
-            continue
-        else:
-            folder_name = os.path.basename(os.path.dirname(c_file))
-            if folder_name == "mod":
-                continue # skip over the file
-            if os.path.basename(c_file) in misc_files:
-                outfile.write("build build/" + os.path.splitext(c_file)[0] + ".c.o: " + "misc_cc " + c_file + "\n")
-            elif os.path.basename(c_file) in optO0_files:
-                outfile.write("build build/" + os.path.splitext(c_file)[0] + ".c.o: " + "O0_cc " + c_file + "\n")
-            elif os.path.basename(c_file) in optO3_files:
-                outfile.write("build build/" + os.path.splitext(c_file)[0] + ".c.o: " + "O3_cc " + c_file + "\n")
-            elif "src/lib" in os.path.relpath(c_file):
-                outfile.write("build build/" + os.path.splitext(c_file)[0] + ".c.o: " + "lib_cc " + c_file + "\n")
-            elif os.path.basename(c_file) == "95F40.c":
-                outfile.write("build build/" + os.path.splitext(c_file)[0] + ".c.o: " + "O2_cc " + c_file + "\n")
-            else:
-                outfile.write("build build/" + os.path.splitext(c_file)[0] + ".c.o: " + "main_cc " + c_file + "\n")
+# Write the rules for .s files
+for s_file in s_files:
+    if "asm/nonmatchings" in s_file:
+        continue
+    if "src/mod" in os.path.relpath(s_file):
+        continue
+    ninja_file.build("build/" + append_extension(s_file), "s_file", s_file)
 
+for bin_file in bin_files:
+    ninja_file.build("build/" + append_extension(bin_file), "bin_file", bin_file)
 
-
-    # Write the rules for the .s files
-    for s_file in s_files:
-        if "asm/nonmatchings" in s_file:
-            continue
-        if "src/mod" in os.path.relpath(s_file):
-            continue
-        outfile.write("build build/" + os.path.splitext(s_file)[0] + ".s.o: " + "s_file " + s_file + "\n")
-
-    # Write the rules for the .bin files
-    for bin_file in bin_files:
-        outfile.write("build build/" + os.path.splitext(bin_file)[0] + ".bin.o: " + "bin_file " + bin_file + "\n")
-
-    # Build the ninja rule with the .o files
-    outfile.write("build build/marioparty.elf: make_elf " + " ".join(o_files) + "\n")
-    outfile.write("build build/marioparty.bin: make_rom_bin build/marioparty.elf\n")
-    outfile.write("build build/marioparty.z64: make_rom_z64 build/marioparty.bin\n")
-    outfile.write("build build/marioparty.ok: make_expected_folder build/marioparty.z64\n")
+ninja_file.build("build/marioparty.elf", "make_elf ", o_files)
+ninja_file.build("build/marioparty.bin", "make_rom_bin ", "build/marioparty.elf")
+ninja_file.build("build/marioparty.z64", "make_rom_z64 ", "build/marioparty.bin")
+ninja_file.build("build/marioparty.ok", "make_expected_folder ", "build/marioparty.z64")
+print ("build.ninja generated")
+ninja_file.close()
