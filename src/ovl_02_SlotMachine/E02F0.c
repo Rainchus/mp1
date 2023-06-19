8a3e79b153b331f4ace259acfd412ba49d320cfb
Rainchus <rainmachus@gmail.com>
2023-06-19 18:00:41 -0500

    add more overlays

---
+++ src/ovl_02_SlotMachine/E02F0.c
@@ -0,0 +1,87 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ovl_02_SlotMachine/E02F0", func_800F65E0_E02F0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_02_SlotMachine/E02F0", func_800F6980_E0690);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_02_SlotMachine/E02F0", func_800F699C_E06AC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_02_SlotMachine/E02F0", func_800F69D0_E06E0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_02_SlotMachine/E02F0", func_800F70A8_E0DB8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FE100_E7E10);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FE104_E7E14);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FE124_E7E34);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FE130_E7E40);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC78_E8988);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC7C_E898C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC80_E8990);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC84_E8994);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC88_E8998);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC8C_E899C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC90_E89A0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC94_E89A4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC98_E89A8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEC9C_E89AC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECA0_E89B0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECA4_E89B4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECA8_E89B8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECAC_E89BC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECB4_E89C4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECB8_E89C8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECBC_E89CC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECC0_E89D0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECC4_E89D4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECC8_E89D8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECCC_E89DC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECD0_E89E0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FECF0_E8A00);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FED50_E8A60);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FED5C_E8A6C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FED68_E8A78);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FED74_E8A84);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FED80_E8A90);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FED8C_E8A9C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEDAC_E8ABC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEDBC_E8ACC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEDE0_E8AF0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_02_SlotMachine/E02F0", D_800FEDF4_E8B04);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_02_SlotMachine/E02F0", func_800F71EC_E0EFC);
---
+++ src/ovl_10_BashNCash/137050.c
@@ -0,0 +1,180 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F65E0_137050);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6B80_1375F0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6BCC_13763C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6D20_137790);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6D6C_1377DC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6EA0_137910);
+
+void func_800F6EE0_137950(void) {
+}
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6EE8_137958);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6F6C_1379DC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F6FD0_137A40);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F7108_137B78);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F711C_137B8C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F7240_137CB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F72BC_137D2C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F73F0_137E60);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3A0_13EE10);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3A4_13EE14);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3A8_13EE18);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3D4_13EE44);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3D8_13EE48);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3DC_13EE4C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3E0_13EE50);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3E4_13EE54);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3E8_13EE58);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3EC_13EE5C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3F0_13EE60);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3F4_13EE64);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3F8_13EE68);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE3FC_13EE6C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE40C_13EE7C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE41C_13EE8C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE420_13EE90);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE424_13EE94);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE428_13EE98);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F7458_137EC8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F7A54_1384C4);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F8240_138CB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F875C_1391CC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F8FD4_139A44);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F99A8_13A418);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F9BAC_13A61C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F9BDC_13A64C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F9D60_13A7D0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F9DD0_13A840);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F9F64_13A9D4);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800F9FD0_13AA40);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FA094_13AB04);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FA120_13AB90);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FA26C_13ACDC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FA440_13AEB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FA5B4_13B024);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FAAE4_13B554);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FB0D8_13BB48);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FB1D4_13BC44);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE458_13EEC8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FB240_13BCB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FB928_13C398);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FB940_13C3B0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FBAB8_13C528);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FBCC8_13C738);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FC240_13CCB0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FC840_13D2B0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FC904_13D374);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FC9D4_13D444);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCBA8_13D618);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCC00_13D670);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCC5C_13D6CC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCCB8_13D728);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCD14_13D784);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCD70_13D7E0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCDCC_13D83C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCE28_13D898);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCE84_13D8F4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE480_13EEF0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE4E4_13EF54);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FCE8C_13D8FC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FD2B0_13DD20);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FD748_13E1B8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FD774_13E1E4);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FD830_13E2A0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FD8E8_13E358);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FDBB4_13E624);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FDED0_13E940);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FDF20_13E990);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FE038_13EAA8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FE158_13EBC8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_10_BashNCash/137050", func_800FE278_13ECE8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE520_13EF90);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_10_BashNCash/137050", D_800FE530_13EFA0);
