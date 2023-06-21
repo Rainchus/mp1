d76f8b76d0407f1c8d205a92d818958de060035a
CreateSource <abnormalhare822@gmail.com>
2023-06-21 19:49:39 -0400

    its so close, 2 fixes max

---
+++ src/ovl_64_ResultsEternalStar2/2B1670.c
@@ -0,0 +1,145 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F65E0_2B1670);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC0C0_2B7150);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC0C4_2B7154);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC0D0_2B7160);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC0DC_2B716C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC0E0_2B7170);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC10C_2B719C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC138_2B71C8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC150_2B71E0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC154_2B71E4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC158_2B71E8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC168_2B71F8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC178_2B7208);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC1A8_2B7238);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC1AC_2B723C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC1B0_2B7240);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC1B8_2B7248);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC1E4_2B7274);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC210_2B72A0);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC218_2B72A8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC224_2B72B4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC244_2B72D4);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC25C_2B72EC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC274_2B7304);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2A0_2B7330);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2A4_2B7334);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2A8_2B7338);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2B8_2B7348);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2C8_2B7358);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2D8_2B7368);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2E8_2B7378);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC2F8_2B7388);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC308_2B7398);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC318_2B73A8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC338_2B73C8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC358_2B73E8);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC370_2B7400);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC374_2B7404);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F678C_2B181C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F6988_2B1A18);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC388_2B7418);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F6C70_2B1D00);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F7324_2B23B4);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F7C28_2B2CB8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F7D94_2B2E24);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC3BC_2B744C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC3C4_2B7454);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F7F1C_2B2FAC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F8990_2B3A20);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F8CFC_2B3D8C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F8EB8_2B3F48);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F9250_2B42E0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F95F8_2B4688);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F972C_2B47BC);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC3CC_2B745C);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC3D4_2B7464);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F9860_2B48F0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800F9C74_2B4D04);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FA08C_2B511C);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FA200_2B5290);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FA3A4_2B5434);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FA5E0_2B5670);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FA61C_2B56AC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FB0A4_2B6134);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FB76C_2B67FC);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FB8E4_2B6974);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FB930_2B69C0);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FBE58_2B6EE8);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FBF78_2B7008);
+
+INCLUDE_ASM("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", func_800FC090_2B7120);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC3F0_2B7480);
+
+INCLUDE_RODATA("asm/nonmatchings/ovl_64_ResultsEternalStar2/2B1670", D_800FC408_2B7498);
