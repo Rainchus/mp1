bb56c533122a684a91f18b377a2ebece16b03fd7
CasiniLoogi <franciscog.21506@gmail.com>
2023-06-23 19:05:42 -0700

    Match func_800F6610_24FAB0 (Thank you Shigure!)

--- src/ovl_3B_MariosRainbowCastle/24FAB0.c
+++ src/ovl_3B_MariosRainbowCastle/24FAB0.c
@@ -1,6 +1,54 @@
 #include "common.h"
+#include "process.h"
 
-INCLUDE_ASM(s32, "ovl_3B_MariosRainbowCastle/24FAB0", func_800F6610_24FAB0);
+void SleepProcess(s32);                                  /* extern */
+
+void func_800F6610_24FAB0(void) {
+    Object* temp_v0;
+    f32 var_f20;
+    f32 var_f22;
+    s32 var_s0;
+    void* temp_s0;
+    void* temp_s2;
+
+    temp_s0 = GetCurrentProcess()->user_data;
+    PlaySound(0x6D);
+    temp_v0 = CreateObject(0x40U, NULL);
+    temp_v0->unk_0A |= 4;
+    func_8004CDCC(temp_v0);
+    func_800A0D50(&temp_v0->coords, temp_s0 + 4);
+    temp_v0->unk_30 = 500.0f;
+    temp_s2 = func_80042728(temp_v0, 0);
+    var_f20 = 0.0f;
+    for(var_s0 = 0; var_s0 < 6; var_s0++) {
+        func_800A0D00(&temp_v0->xScale, var_f20, var_f20, var_f20);
+        var_f20 += 0.4f;
+        SleepVProcess();
+    }
+    for(var_s0 = 0; var_s0 < 3; var_s0++) {
+        func_800A0D00(&temp_v0->xScale, var_f20, var_f20, var_f20);
+        var_f20 -= 0.4f;
+        SleepVProcess();
+    }
+    SleepProcess(0x1E);
+    PlaySound(0x44);
+    var_f22 = 0.0f;
+    while (TRUE) {
+        func_800A40D0(D_800F2B7C[*temp_v0->unk_3C->unk_40].unk7C[0], var_f22);
+        var_f22 += 20.0f;
+        var_f20 -= 0.02f;
+        if (var_f20 < 0.0f) {
+            break;
+        }
+        func_800A0D00(&temp_v0->xScale, var_f20, var_f20, var_f20);
+        temp_v0->unk_30 -= 6.0f;
+        SleepVProcess();
+    }
+    func_800427D4(temp_s2);
+    SleepProcess(0x1E);
+    DestroyObject(temp_v0);
+    EndProcess(NULL);
+}
 
 INCLUDE_ASM(s32, "ovl_3B_MariosRainbowCastle/24FAB0", func_800F6820_24FCC0);
 
