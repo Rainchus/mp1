08a5cabf56967f48df944adc2654bbed39011db4
Rainchus <rainmachus@gmail.com>
2024-10-17 08:09:49 -0500

    fix alignment of some cod

--- src/engine/spaces.c
+++ src/engine/spaces.c
@@ -3,57 +3,57 @@
 
 
 void LoadSpaceTextures(s16 type) {
-    int i;
-    u32 *ptr;
+   int i;
+   u32 *ptr;
 
-    D_800D8140 = type;
-    switch (type) {
-        case 0:
-            ptr = D_800C4FD4;
-            break;
-        case 1:
-            ptr = D_800C4FFC;
-            break;
-        default:
-            ptr = D_800C5024;
-            break;
-    }
+   D_800D8140 = type;
+   switch (type) {
+      case 0:
+         ptr = D_800C4FD4;
+         break;
+      case 1:
+         ptr = D_800C4FFC;
+         break;
+      default:
+         ptr = D_800C5024;
+         break;
+   }
 
-    for (i = 0; i < SPACE_TYPE_TOTAL; i++) {
-        if (ptr[i] != 0) {
-            D_800D8118[i] = ReadMainFS(ptr[i]);
-        }
-        else {
-            D_800D8118[i] = NULL;
-        }
-    }
+   for (i = 0; i < SPACE_TYPE_TOTAL; i++) {
+      if (ptr[i] != 0) {
+         D_800D8118[i] = ReadMainFS(ptr[i]);
+      }
+      else {
+         D_800D8118[i] = NULL;
+      }
+   }
 }
 
 /* HuMemMemoryFree D_800D8118. */
 void FreeSpaceTextures(void) {
-    s32 i;
-    for (i = 0; i < SPACE_TYPE_TOTAL; i++) {
-        if (D_800D8118[i] != NULL) {
-            FreeMainFS(D_800D8118[i]);
-        }
-        D_800D8118[i] = NULL;
-    }
+   s32 i;
+   for (i = 0; i < SPACE_TYPE_TOTAL; i++) {
+      if (D_800D8118[i] != NULL) {
+         FreeMainFS(D_800D8118[i]);
+      }
+      D_800D8118[i] = NULL;
+   }
 }
 
 /* Set board as 0? */
 void LoadInitialSpaceTextures(void) {
-    LoadSpaceTextures(0);
-    D_800C4FD0 = NULL;
+   LoadSpaceTextures(0);
+   D_800C4FD0 = NULL;
 }
 
 void FreeSpaceTexturesWrapper(void) {
-    FreeSpaceTextures();
+   FreeSpaceTextures();
 }
 
 /* HuMemMemoryFree and then set D_800D8118 */
 void ChangeSpaceTextures(s16 type) {
-    FreeSpaceTextures();
-    LoadSpaceTextures(type);
+   FreeSpaceTextures();
+   LoadSpaceTextures(type);
 }
 
 /* Rendering */
@@ -63,87 +63,87 @@ INCLUDE_ASM("asm/nonmatchings/engine/spaces", RenderSpaces);
 
 /* Get pointer to space data section */
 u8 *GetSpaceDataStream(u8 *byteSteam, s32 metaDataOffset) {
-    u16* pDataOffset = (u16*) &byteSteam[metaDataOffset];
-    return &byteSteam[*pDataOffset];
+   u16* pDataOffset = (u16*) &byteSteam[metaDataOffset];
+   return &byteSteam[*pDataOffset];
 }
 
 void func_80028E8C(s16, void*); // Unk
 /* Load Board Related Data From File */
 s32 LoadBoardSpaces(s16 dir, s16 file) {
-    //struct board_def *boarddef;
-    u16* pDataStream;
-    SpaceData *pSpaceData;
-    ChainData *pChainData;
-    u8* chainOffsets;
-    s16* chainValues;
-    s32 i, j;
+   //struct board_def *boarddef;
+   u16* pDataStream;
+   SpaceData *pSpaceData;
+   ChainData *pChainData;
+   u8* chainOffsets;
+   s16* chainValues;
+   s32 i, j;
 
-    D_800C4FD0 = ReadMainFS((dir << 16) | file);
-    if (D_800C4FD0 != NULL) {
-        /* Reset special space event lists */
-        D_800D8144 = NULL;
-        D_800D8148 = NULL;
-        D_800D814C = NULL;
-        D_800D8150 = NULL;
-        pDataStream = (u16*) D_800C4FD0;
-        D_800D8100 = *pDataStream++;
-        D_800D8102 = *pDataStream++;
-        D_800D8104 = *pDataStream++;
+   D_800C4FD0 = ReadMainFS((dir << 16) | file);
+   if (D_800C4FD0 != NULL) {
+      /* Reset special space event lists */
+      D_800D8144 = NULL;
+      D_800D8148 = NULL;
+      D_800D814C = NULL;
+      D_800D8150 = NULL;
+      pDataStream = (u16*) D_800C4FD0;
+      D_800D8100 = *pDataStream++;
+      D_800D8102 = *pDataStream++;
+      D_800D8104 = *pDataStream++;
 
-        /* Load space data */
-        D_800D8108 = (SpaceData*) MallocTemp(D_800D8100 * sizeof(SpaceData));
-        pDataStream = (u16*) GetSpaceDataStream(D_800C4FD0, 6);
-        for (i = 0, pSpaceData = D_800D8108; i < D_800D8100; i++, pSpaceData++) {
-            Vec3f *pos;
-            pSpaceData->unk0 = 1;
-            pSpaceData->unk2 = *pDataStream++;
-            pSpaceData->spaceType = *pDataStream++;
-            pos = (Vec3f*) pDataStream;
-            pSpaceData->coords.x = pos->x * 5.0f;
-            pSpaceData->coords.y = pos->y * 5.0f;
-            pSpaceData->coords.z = pos->z * 5.0f;
-            pDataStream = (u16*) (++pos);
-            pSpaceData->sx = 1.0f;
-            pSpaceData->sy = 1.0f;
-            pSpaceData->sz = 1.0f;
-            pSpaceData->eventList = NULL;
-        }
+      /* Load space data */
+      D_800D8108 = (SpaceData*) MallocTemp(D_800D8100 * sizeof(SpaceData));
+      pDataStream = (u16*) GetSpaceDataStream(D_800C4FD0, 6);
+      for (i = 0, pSpaceData = D_800D8108; i < D_800D8100; i++, pSpaceData++) {
+         Vec3f *pos;
+         pSpaceData->unk0 = 1;
+         pSpaceData->unk2 = *pDataStream++;
+         pSpaceData->spaceType = *pDataStream++;
+         pos = (Vec3f*) pDataStream;
+         pSpaceData->coords.x = pos->x * 5.0f;
+         pSpaceData->coords.y = pos->y * 5.0f;
+         pSpaceData->coords.z = pos->z * 5.0f;
+         pDataStream = (u16*) (++pos);
+         pSpaceData->sx = 1.0f;
+         pSpaceData->sy = 1.0f;
+         pSpaceData->sz = 1.0f;
+         pSpaceData->eventList = NULL;
+      }
 
-        /* Load chain data 1 */
-        D_800D810C = (ChainData*) MallocTemp(D_800D8102 * sizeof(ChainData));
-        chainOffsets = GetSpaceDataStream(D_800C4FD0, 8);
-        for (i = 0, pChainData = D_800D810C; i < D_800D8102; i++, pChainData++) {
-            pDataStream = (u16*) GetSpaceDataStream(chainOffsets, i * 2);
-            pChainData->len = *pDataStream;
-            pDataStream++;
+      /* Load chain data 1 */
+      D_800D810C = (ChainData*) MallocTemp(D_800D8102 * sizeof(ChainData));
+      chainOffsets = GetSpaceDataStream(D_800C4FD0, 8);
+      for (i = 0, pChainData = D_800D810C; i < D_800D8102; i++, pChainData++) {
+         pDataStream = (u16*) GetSpaceDataStream(chainOffsets, i * 2);
+         pChainData->len = *pDataStream;
+         pDataStream++;
 
-            pChainData->spaceIndices = (s16*)MallocTemp( (s16) pChainData->len * sizeof(s16));
-            chainValues = pChainData->spaceIndices;
-            for(j = 0; j < (s16) pChainData->len; j++) {
-                *chainValues++ = *pDataStream++;
-            }
-        }
+         pChainData->spaceIndices = (s16*)MallocTemp( (s16) pChainData->len * sizeof(s16));
+         chainValues = pChainData->spaceIndices;
+         for(j = 0; j < (s16) pChainData->len; j++) {
+               *chainValues++ = *pDataStream++;
+         }
+      }
 
-        /* Load chain data 2 */
-        D_800D8110 = (ChainData*) MallocTemp(D_800D8104  * sizeof(ChainData));
-        chainOffsets = GetSpaceDataStream(D_800C4FD0, 10);
-        for (i = 0, pChainData = D_800D8110; i < D_800D8104; i++, pChainData++) {
-            pDataStream = (u16*) GetSpaceDataStream(chainOffsets, i * 2);
-            pChainData->len = *pDataStream;
-            pDataStream++;
+      /* Load chain data 2 */
+      D_800D8110 = (ChainData*) MallocTemp(D_800D8104  * sizeof(ChainData));
+      chainOffsets = GetSpaceDataStream(D_800C4FD0, 10);
+      for (i = 0, pChainData = D_800D8110; i < D_800D8104; i++, pChainData++) {
+         pDataStream = (u16*) GetSpaceDataStream(chainOffsets, i * 2);
+         pChainData->len = *pDataStream;
+         pDataStream++;
 
-            pChainData->spaceIndices = (s16*)MallocTemp( (s16)pChainData->len * sizeof(s16));
-            chainValues = pChainData->spaceIndices;
-            for(j = 0; j < (s16) pChainData->len; j++) {
-                *chainValues++ = *pDataStream++;
-            }
-        }
+         pChainData->spaceIndices = (s16*)MallocTemp( (s16)pChainData->len * sizeof(s16));
+         chainValues = pChainData->spaceIndices;
+         for(j = 0; j < (s16) pChainData->len; j++) {
+               *chainValues++ = *pDataStream++;
+         }
+      }
 
-        FreeMainFS(D_800C4FD0);
-        func_80028E8C(1, RenderSpaces);
-        D_800F3290 = 1;
-    }
-    return 0;
+      FreeMainFS(D_800C4FD0);
+      func_80028E8C(1, RenderSpaces);
+      D_800F3290 = 1;
+   }
+   return 0;
 }
 
 /* HuMemMemoryFree board temps */
@@ -174,25 +174,25 @@ void FreeBoardSpaces(void) {
 }
 
 SpaceData *GetSpaceData(s16 index) {
-    return &D_800D8108[index];
+   return &D_800D8108[index];
 }
 
 s16 GetAbsSpaceIndexFromChainSpaceIndex(u16 chainIndex, u16 spaceIndex) {
-    return D_800D8110[chainIndex].spaceIndices[spaceIndex];
+   return D_800D8110[chainIndex].spaceIndices[spaceIndex];
 }
 
 s16 GetChainLength(u16 chainIndex) {
-    return D_800D8110[chainIndex].len;
+   return D_800D8110[chainIndex].len;
 }
 
 s16 GetChainSpaceIndexFromAbsSpaceIndex(s16 absIndex, s32 chainIndex) {
-    s32 i;
-    for (i = 0; i < GetChainLength(chainIndex); i++) {
-        if (GetAbsSpaceIndexFromChainSpaceIndex(chainIndex, i) == absIndex) {
-            return i;
-        }
-    }
-    return SPACE_INDEX_INVALID;
+   s32 i;
+   for (i = 0; i < GetChainLength(chainIndex); i++) {
+      if (GetAbsSpaceIndexFromChainSpaceIndex(chainIndex, i) == absIndex) {
+         return i;
+      }
+   }
+   return SPACE_INDEX_INVALID;
 }
 
 s16 GetRandomSpaceOfTypeInChain(u16 type, u16 chainIndex) {
@@ -249,27 +249,27 @@ s16 GetRandomSpaceOfType(u16 type) {
 
 
 void SetSpaceType(s16 spaceIndex, u8 spaceType) {
-    SpaceData *space;
+   SpaceData *space;
 
-    space = GetSpaceData(spaceIndex);
-    space->spaceType = spaceType;
+   space = GetSpaceData(spaceIndex);
+   space->spaceType = spaceType;
 }
 
 /* Change spaces of old type to new type on a given chain */
 void SetSpaceTypeInChain(u16 chainIndex, u16 oldType, u8 newType) {
-    s32 chainLen;
-    s16 absIndex;
-    SpaceData *space;
-    s32 i;
+   s32 chainLen;
+   s16 absIndex;
+   SpaceData *space;
+   s32 i;
 
-    chainLen = GetChainLength(chainIndex);
-    for (i = 0; i < chainLen; i++) {
-        absIndex = GetAbsSpaceIndexFromChainSpaceIndex(chainIndex, i);
-        space = GetSpaceData(absIndex);
-        if (space->spaceType == oldType) {
-            space->spaceType = newType;
-        }
-    }
+   chainLen = GetChainLength(chainIndex);
+   for (i = 0; i < chainLen; i++) {
+      absIndex = GetAbsSpaceIndexFromChainSpaceIndex(chainIndex, i);
+      space = GetSpaceData(absIndex);
+      if (space->spaceType == oldType) {
+         space->spaceType = newType;
+      }
+   }
 }
 
 /* Space process */
@@ -375,28 +375,27 @@ void SetSpaceSpawnAnim(s16 spaceIndex) {
 }
 
 void SetSpaceEventList(s16 index, EventListEntry *eventList) {
+   SpaceData *space;
+
    /* Event index. */
    switch (index) {
-      case EVENT_INDEX_NEWTURN:
-         D_800D8144 = eventList;
-         return;
-      case EVENT_INDEX_UNUSED:
-         D_800D8148 = eventList;
-         return;
-      case EVENT_INDEX_PLAYERTURN:
-         D_800D814C = eventList;
-         return;
-      case EVENT_INDEX_PLAYERDICE:
-         D_800D8150 = eventList;
-         return;
-    }
+   case EVENT_INDEX_NEWTURN:
+      D_800D8144 = eventList;
+      return;
+   case EVENT_INDEX_UNUSED:
+      D_800D8148 = eventList;
+      return;
+   case EVENT_INDEX_PLAYERTURN:
+      D_800D814C = eventList;
+      return;
+   case EVENT_INDEX_PLAYERDICE:
+      D_800D8150 = eventList;
+      return;
+   }
 
-    {
-      /* Default is event from space index. */
-      SpaceData *space;
-      space = GetSpaceData(index);
-      space->eventList = eventList;
-    }
+   /* Default is event from space index. */
+   space = GetSpaceData(index);
+   space->eventList = eventList;
 }
 
 void EventTableHydrate(EventTableEntry *table) {
@@ -465,18 +464,18 @@ s32 ExecuteEventForSpace(s16 index, s16 activationType) {
 
 /* Set space event global return flags. */
 void SetEventReturnFlag(s32 flags) {
-    D_800D8154 = flags;
+   D_800D8154 = flags;
 }
 
 void SetCurrentSpaceIndex(s16 spaceIndex) {
-    D_800ED5E0 = spaceIndex;
+   D_800ED5E0 = spaceIndex;
 }
 
 s16 GetCurrentSpaceIndex(void) {
-    return D_800ED5E0;
+   return D_800ED5E0;
 }
 
 /* Pick random chance time space. */
 s16 GetRandomChanceSpace(void) {
-    return GetRandomSpaceOfType(SPACE_TYPE_CHANCE);
+   return GetRandomSpaceOfType(SPACE_TYPE_CHANCE);
 }
